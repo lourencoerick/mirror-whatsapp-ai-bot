72b6e1a44e4db65bfadd441b0a8cdd0f1679f4a1
Erick Lourenco <lourenco.erick@gmail.com>
2025-02-20 21:00:19 +0000

    chore: update the file to use absolute path

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, DateTime
 from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.sql import func
 
-from ..database import Base
+from app.database import Base
 
 
 class BaseModel(Base):
