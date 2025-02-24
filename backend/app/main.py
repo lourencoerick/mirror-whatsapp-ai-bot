c338a5d123cc5ead7cd35dbfd21f61ce7c59c769
Erick Lourenco <lourenco.erick@gmail.com>
2025-02-24 21:39:17 +0000

    feat(backend): Add middleware and authentication routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,41 @@
+import os
 from fastapi import FastAPI
+from starlette.middleware.sessions import SessionMiddleware
+from fastapi.middleware.cors import CORSMiddleware
+from app.api.auth import router as auth_router
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Create an instance of the FastAPI class
 app = FastAPI()
+
+# Get the frontend domain from environment variables
+frontend_domain = os.getenv("FRONTEND_DOMAIN", "http://localhost:3001")
+secret_key = os.getenv("SECRET_KEY", "my_secret_key")
+
+# Add session middleware
+app.add_middleware(
+    SessionMiddleware, secret_key=secret_key, same_site="lax", https_only=False
+)
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[frontend_domain],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Include the authentication router
+app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
+
+
+@app.get("/")
+def home():
+    return {"message": "API FastAPI com Auth0"}
 
 
 @app.get("/health")
