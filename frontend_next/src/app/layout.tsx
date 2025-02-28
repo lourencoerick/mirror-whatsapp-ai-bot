d2b0723afc5eca4e864a5ff3f18e23df8501348d
Erick Lourenco <lourenco.erick@gmail.com>
2025-02-28 22:00:39 +0000

    fix: put the children inside ThemeProvider

--- frontend_next/src/app/layout.tsx
+++ frontend_next/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,16 +25,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
-        ></ThemeProvider>
-        {children}
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
