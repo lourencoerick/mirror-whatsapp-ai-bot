580c63617f1367a85cf2fed8a0b43994bf645169
Erick Lourenco <lourenco.erick@gmail.com>
2025-03-05 00:41:11 +0000

    refact: create a content dict to make the maintenance easier

--- frontend_next/src/components/ui/home/benefits-section.tsx
+++ frontend_next/src/components/ui/home/benefits-section.tsx
@@ -1,88 +1,94 @@
-import Card from "./feature-card";
 import { InteractiveHoverButton } from "@/components/magicui/interactive-hover-button";
 import { AnimatedListDemo } from "@/components/ui/home/messages-poping-up";
 import { Safari } from "@/components/magicui/safari";
 
-export default function BenefitsSection() {
+const content = {
+    firstArgument: {
+        title: "Lambda Labs desenvolveu vendedores I.A. que não apenas respondem, mas persuadem.",
+        description: "Fazendo uso de qualificação automática de leads, contorno de objeções e fechamento inteligente – enquanto você foca no crescimento.",
+        buttonText: "Quero os melhores vendedores em minha operação",
+        videoSrc: "/jess.mp4",
+        videoAlt: "Your browser does not support the video tag.",
+    },
+    secondArgument: {
+        title: "Na dúvida, confie nos números – e venda com mais segurança.",
+        description: `Na Lambda Labs, você tem à disposição um <span class="font-bold">painel de métricas completo</span> e a possibilidade de rodar testes constantemente. Isso significa entender, em tempo real, <span class="font-bold">o que gera resultados e o que precisa ser ajustado</span>.`,
+        buttonText: "Quero descobrir o que funciona com meus leads",
+        imageSrc: "/dashboard.png",
+        imageAlt: "app.lambdalabs.com.br",
+    },
+    thirdArgument: {
+        title: "Afogado em mensagens? Seu cliente não vai esperar!",
+        description: `Clientes perguntam, negociam e querem resposta <span class="font-bold">na hora</span>. Enquanto você tenta responder um, <span class="font-bold">outros já foram embora</span>.`,
+        listItems: [
+            "Quanto mais mensagens acumulam, mais vendas você perde.",
+            "Gerenciar a caixa de entrada se torna uma missão impossível.",
+            "O WhatsApp não para, mas você precisa respirar.",
+        ],
+        buttonText: "Quero automatizar minhas vendas",
+    },
+};
 
+export default function BenefitsSection() {
     return (
         <section className="min-h-screen bg-secondary text-secondary-foreground flex flex-col items-center px-6">
-            <h1 className="text-2xl md:text-4xl text-center mt-8 mb-10 md:mb-20 ">Você pode continuar sobrecarregado... ou deixar a I.A. vender para você</h1>
+            <h1 className="text-2xl md:text-4xl text-center mt-8 mb-10 md:mb-20">Você pode continuar sobrecarregado... ou deixar a I.A. vender para você</h1>
             <div className="flex flex-col gap-30">
                 <div id="first-argument">
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-10  justify-center">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-10 justify-center">
                         <div>
-                            <div className="flex flex-col  md:mt-5 mb-5">
-                                <h2 className="max-w-lg text-2xl md:text-4xl text-start leading-relaxed">Lambda Labs desenvolveu vendedores I.A. que não apenas respondem, mas persuadem.</h2>
-                                <p className="max-w-lg text-md md:text-lg mt-2">Fazendo uso de qualificação automática de leads, contorno de objeções e fechamento inteligente – enquanto você foca no crescimento.</p>
-
-
-
+                            <div className="flex flex-col md:mt-5 mb-5">
+                                <h2 className="max-w-lg text-2xl md:text-4xl text-start leading-relaxed">{content.firstArgument.title}</h2>
+                                <p className="max-w-lg text-md md:text-lg mt-2">{content.firstArgument.description}</p>
                             </div>
-                            <InteractiveHoverButton className="hidden md:block max-w-lg text-md">Quero os melhores vendedores em minha operação</InteractiveHoverButton>
+                            <InteractiveHoverButton className="hidden md:block max-w-lg text-md">{content.firstArgument.buttonText}</InteractiveHoverButton>
                         </div>
-
                         <div className="max-w-lg flex flex-col justify-center items-center">
                             <video className="rounded-xl" preload="none" autoPlay muted loop>
-                                <source src="/jess.mp4" type="video/mp4" />
-                                Your browser does not support the video tag.
+                                <source src={content.firstArgument.videoSrc} type="video/mp4" />
+                                {content.firstArgument.videoAlt}
                             </video>
-                            <InteractiveHoverButton className="block md:hidden max-w-sm text-xl mt-10">Quero automatizar minhas vendas</InteractiveHoverButton>
+                            <InteractiveHoverButton className="block md:hidden max-w-sm text-xl mt-10">{content.firstArgument.buttonText}</InteractiveHoverButton>
                         </div>
-
                     </div>
                 </div>
 
-
                 <div id="second-argument" className="">
-
-
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-10  justify-center">
-                        <div className="relative">
-
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-10 justify-center">
+                        <div className="order-2 md:order-1 relative">
                             <Safari
-                                url="app.lambdalabs.com.br"
-                                className="max-w-lg h-fit object-cover"// aspect-video"-mt-45 
-                                imageSrc="/dashboard.png"
+                                url={content.secondArgument.imageAlt}
+                                className="max-w-md md:max-w-lg h-fit object-cover"
+                                imageSrc={content.secondArgument.imageSrc}
                             />
                         </div>
-                        <div>
+                        <div className="order-1 md:order-2">
                             <div className="flex flex-col mb-5">
-                                <h2 className="max-w-lg text-2xl md:text-4xl text-start leading-relaxed">Na dúvida, confie nos números – e venda com mais segurança.</h2>
-                                <p className="max-w-lg text-md md:text-lg mt-2">Na Lambda Labs, você tem à disposição um <span className="font-bold">painel de métricas completo</span> e a possibilidade de rodar testes constantemente. Isso significa entender, em tempo real, <span className="font-bold">o que gera resultados e o que precisa ser ajustado</span>.</p>
+                                <h2 className="max-w-lg text-2xl md:text-4xl text-start leading-relaxed">{content.secondArgument.title}</h2>
+                                <p className="max-w-lg text-md md:text-lg mt-2" dangerouslySetInnerHTML={{ __html: content.secondArgument.description }}></p>
                             </div>
-                            <InteractiveHoverButton className="hidden md:block max-w-lg text-md">Quero descobrir o que funciona com meus leads</InteractiveHoverButton>
+                            <InteractiveHoverButton className="hidden md:block max-w-lg text-md">{content.secondArgument.buttonText}</InteractiveHoverButton>
                         </div>
-
                     </div>
                 </div>
 
                 <div id="third-argument" className="">
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-10  justify-center">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-10 justify-center">
                         <div>
-                            <div className="flex flex-col  md:mt-5 mb-5">
-                                <h2 className="max-w-lg text-2xl md:text-4xl text-start leading-relaxed">Afogado em mensagens? Seu cliente não vai esperar!</h2>
-                                <p className="max-w-lg text-md md:text-lg mt-2">Clientes perguntam, negociam e querem resposta <span className="font-bold">na agora</span>. Enquanto você tenta responder um, <span className="font-bold">outros já foram embora</span>.</p>
+                            <div className="flex flex-col md:mt-5 mb-5">
+                                <h2 className="max-w-lg text-2xl md:text-4xl text-start leading-relaxed">{content.thirdArgument.title}</h2>
+                                <p className="max-w-lg text-md md:text-lg mt-2" dangerouslySetInnerHTML={{ __html: content.thirdArgument.description }}></p>
                                 <ul className="list-disc pl-5 space-y-2">
-                                    <li>Quanto mais mensagens acumulam, mais vendas você perde.</li>
-                                    <li>Gerenciar a caixa de entrada se torna uma missão impossível.</li>
-                                    <li>O WhatsApp não para, mas você precisa respirar.</li>
+                                    {content.thirdArgument.listItems.map((item, index) => (
+                                        <li key={index}>{item}</li>
+                                    ))}
                                 </ul>
-
-
-
                             </div>
-                            <InteractiveHoverButton className="hidden md:block max-w-sm text-xl">Quero automatizar minhas vendas</InteractiveHoverButton>
+                            <InteractiveHoverButton className="hidden md:block max-w-lg text-md">{content.thirdArgument.buttonText}</InteractiveHoverButton>
                         </div>
-
-
-                        <AnimatedListDemo></AnimatedListDemo>
-
+                        <AnimatedListDemo />
                     </div>
-
                 </div>
-
-
             </div>
         </section>
     );
