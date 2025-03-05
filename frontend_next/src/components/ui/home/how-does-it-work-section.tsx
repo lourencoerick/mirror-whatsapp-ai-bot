14219a216d443e0c8c934fe55bf1ab419f5c1d5b
Erick Lourenco <lourenco.erick@gmail.com>
2025-03-05 00:41:32 +0000

    feat: add how does it work section

---
+++ frontend_next/src/components/ui/home/how-does-it-work-section.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { LogIn, Handshake, Gauge } from 'lucide-react';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import StepCard from './cards';
+
+
+const steps = [
+    {
+        step: 1,
+        title: 'Crie sua conta',
+        titleSize: "text-2xl",
+        icon: <LogIn className="inline align-middle text-muted-foreground" size={25} />,
+        description: (
+            <>
+                Faça login com sua conta <span className="font-bold">Facebook</span>, e conecte-se ao{' '}
+                <span className="font-bold">Whatsapp Business</span>.<br />
+                <br />Ou cadastre-se com e-mail e senha.
+            </>
+        ),
+        descriptionMargin: "mt-10"
+    },
+    {
+        step: 2,
+        title: 'Apresente sua marca ao seu novo vendedor I.A.',
+        titleSize: "text-lg",
+        icon: <Handshake className="inline align-middle text-muted-foreground" size={25} />,
+        description: (
+            <>
+                
+                <span className="font-bold">Compartilhe a essência do seu negócio</span>, descrevendo sua marca, missão e valores.{' '}
+                <br /><br />Adicione também seu <span className="font-bold">catálogo de produtos e serviços</span> para um onboarding completo.
+            </>
+        ),
+        descriptionMargin: "mt-6"
+    },
+    {
+        step: 3,
+        title: 'Dê o sinal verde – seu vendedor I.A está pronto',
+        titleSize: "text-lg",
+        icon: <WhatsAppIcon className="inline align-middle text-muted-foreground" size={30} />,
+        description: (
+            <>
+                Seu novo funcionário I.A está pronto para assumir a <span className="font-bold">operação de vendas 24/7</span>.{' '}
+                <br /><br />Habilite-o e <span className="font-bold">nunca mais deixe um lead sem resposta</span>.
+            </>
+        ),
+        descriptionMargin: "mt-6"
+    },    
+    {
+        step: 4,
+        title: 'Monitore. Otimize. Venda Mais.',
+        titleSize: "text-lg",
+        icon: <Gauge className="inline align-middle text-muted-foreground" size={25} />,
+        description: (
+            <>
+                No painel de controle, você pode <span className="font-bold">acompanhar conversões e vendas via WhatsApp</span>,{' '}
+                bem como <span className="font-bold">analisar as conversas</span> entre os leads e seu vendedor I.A.
+       
+            </>
+        ),
+        descriptionMargin: "mt-6"
+    },   
+
+    
+];
+
+export default function HowDoesItWorkSection() {
+    return (
+        // <Button variant="contained">Hello world</Button>
+        
+        <div className="bg-background text-background-foreground">
+            <h1 className="text-4xl text-center mt-12 mb-10">Como Funciona</h1>
+            <div className="flex flex-row gap-8 p-8">
+                {steps.map(({ step, title, titleSize, icon, description, descriptionMargin}, index) => (
+                    <StepCard key={index} step={step} title={title} titleSize={titleSize} icon={icon} description={description} descriptionMargin={descriptionMargin}/>
+                ))}
+            </div>
+        </div>
+        
+    );
+}
