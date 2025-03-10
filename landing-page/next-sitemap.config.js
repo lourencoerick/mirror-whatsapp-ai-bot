1c7a7e05539e6fa3315d0b52e693e32f2c079e8d
Erick Lourenco <lourenco.erick@gmail.com>
2025-03-10 18:48:33 +0000

    feat: config the next-sitemap

---
+++ landing-page/next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: 'https://www.lambdalabs.com.br', // URL do seu site
+    generateRobotsTxt: true, // Gera automaticamente um robots.txt
+    sitemapSize: 5000,
+  };
+  
