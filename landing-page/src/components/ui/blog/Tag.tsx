d5432d80ac2279c3a2b9b9e451e51b76a52eb39c
Erick Lourenco <lourenco.erick@gmail.com>
2025-03-14 01:42:41 +0000

    fix: fix the href of the link

--- landing-page/src/components/ui/blog/Tag.tsx
+++ landing-page/src/components/ui/blog/Tag.tsx
@@ -7,8 +7,8 @@ interface Props {
 const Tag = ({ text }: Props) => {
   return (
     <Link
-      href={`tags/${slug(text)}`}
-      className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400 mr-3 text-sm font-medium uppercase"
+      href={`/blog/tags/${slug(text)}`}
+      className="hover:text-foreground text-muted-foreground mr-3 text-sm font-medium uppercase"
     >
       {text.split(' ').join('-')}
     </Link>
