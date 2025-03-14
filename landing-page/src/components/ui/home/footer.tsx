6d202df18d949e521028da838cf1f656abf2481f
Erick Lourenco <lourenco.erick@gmail.com>
2025-03-14 15:25:23 +0000

    feat: use siteMetadata and fix the max height

--- landing-page/src/components/ui/home/footer.tsx
+++ landing-page/src/components/ui/home/footer.tsx
@@ -1,29 +1,19 @@
 // components/Footer.tsx
 import Link from "next/link";
+import siteMetadata from "@/data/siteMetadata";
 
 export default function Footer() {
   return (
-    <footer className="bg-background text-muted-foreground border-t border-border py-8">
+    <footer className="bg-background text-muted-foreground border-t border-border py-8 max-h-fit-content">
       <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
         <div className="mb-4 md:mb-0 text-center md:text-left">
           <Link href="/">
-            <span className="text-lg text-primary font-semibold ">Lambda Labs</span>
+            <span className="text-lg text-primary font-semibold ">{siteMetadata.headerTitle}</span>
           </Link>
           <p className="text-sm text-gray-500">
-            © {new Date().getFullYear()} Lambda Labs. Todos os direitos reservados.
+            © {new Date().getFullYear()} {siteMetadata.headerTitle}. Todos os direitos reservados.
           </p>
         </div>
-        {/* <div className="flex space-x-4">
-          <Link href="/about">
-            <span className="text-sm hover:text-accent-foreground">Sobre</span>
-          </Link>
-          <Link href="/contact">
-            <span className="text-sm  hover:text-accent-foreground">Contato</span>
-          </Link>
-          <Link href="/privacy">
-            <span className="text-sm  hover:text-accent-foreground">Privacidade</span>
-          </Link>
-        </div> */}
       </div>
     </footer>
   );
