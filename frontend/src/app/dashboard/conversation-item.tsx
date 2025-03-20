cc58793b1778463383973cf9db26e77d47d636e3
Erick Lourenco <lourenco.erick@gmail.com>
2025-03-20 17:03:26 +0000

    feat: add link to the conversation item

--- frontend/src/app/dashboard/conversation-item.tsx
+++ frontend/src/app/dashboard/conversation-item.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { User } from 'lucide-react'
-
+import Link from 'next/link';
+import clsx from 'clsx';
 
 
 type Props = {
@@ -11,27 +12,36 @@ type Props = {
   lastMessage: string
   lastMessageTime: string
   imageUrl: string
+  isSelected: boolean
 }
 
 
 const ConversationItem: React.FC<Props> = (props: Props) => {
   return (
-    <div className='flex flex-row items-center px-2 gap-2 truncate border-t border-gray-200 h-20'>
-      <Avatar>
-        <AvatarImage src={props.imageUrl} alt={props.name} />
-        <AvatarFallback>
-          <User />
-        </AvatarFallback>
-      </Avatar>
-      <div className='flex flex-col flex-1'>
-        <div className='flex flex-row justify-between gap-2'>
-          <h4 className='text-sm truncate'><span>{`${props.phoneNumber} | ${props.name}`} </span></h4>
-          <p className='text-sm text-muted-foreground truncate'>{props.lastMessageTime}</p>
-        </div>
 
-        <p className='text-sm text-muted-foreground truncate'>{props.lastMessage}</p>
+    <Link href={`/dashboard/conversations/${props.id}`} className="block">
+      <div
+        className={clsx(
+          'flex flex-row items-center px-2 gap-2 truncate border-t border-gray-200 h-20',
+          props.isSelected ? 'bg-background' : 'hover:bg-gray-100'
+        )}
+      >
+        <Avatar>
+          <AvatarImage src={props.imageUrl} alt={props.name} />
+          <AvatarFallback>
+            <User />
+          </AvatarFallback>
+        </Avatar>
+        <div className='flex flex-col flex-1'>
+          <div className='flex flex-row justify-between gap-2'>
+            <h4 className='text-sm truncate'><span>{`${props.phoneNumber} | ${props.name}`} </span></h4>
+            <p className='text-sm text-muted-foreground truncate'>{props.lastMessageTime}</p>
+          </div>
+
+          <p className='text-sm text-muted-foreground truncate'>{props.lastMessage}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
