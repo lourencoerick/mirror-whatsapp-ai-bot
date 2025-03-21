f2a40777ccb7916b6b87c01a2be2c45040db4acb
Erick Lourenco <lourenco.erick@gmail.com>
2025-03-21 00:43:09 +0000

    docs: add docstrings

--- backend/app/services/queue/interface.py
+++ backend/app/services/queue/interface.py
@@ -2,11 +2,14 @@ from abc import ABC, abstractmethod
 
 
 class IQueue(ABC):
+    """Interface for message queue implementations."""
 
     @abstractmethod
     def enqueue(self, message: dict) -> None:
+        """Add a message to the queue."""
         pass
 
     @abstractmethod
     def dequeue(self) -> dict:
+        """Consume the next message from the queue."""
         pass
