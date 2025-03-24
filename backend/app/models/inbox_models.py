0881ec84131e8c407bbb0619bc71dda652269d5c
Erick Lourenco <lourenco.erick@gmail.com>
2025-03-24 13:12:07 +0000

    feat: review inbox models

--- backend/app/models/inbox_models.py
+++ backend/app/models/inbox_models.py
@@ -1,16 +1,26 @@
-from sqlalchemy import Column, Integer, String, SmallInteger, ForeignKey, Index
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Boolean,
+    ForeignKey,
+    Index,
+    UniqueConstraint,
+)
 from sqlalchemy.orm import relationship
 from app.models.base import BaseModel
 
 
 class Inbox(BaseModel):
     __tablename__ = "inboxes"
+    id = Column(Integer, primary_key=True, autoincrement=True)
     __table_args__ = (Index("inboxes_account_id_index", "account_id"),)
+
     account_id = Column(Integer, ForeignKey("accounts.id"), nullable=False)
     channel_id = Column(Integer, nullable=False)
     name = Column(String(255), nullable=False)
     channel_type = Column(String(255), nullable=True)
-    enable_auto_assignment = Column(SmallInteger, nullable=True)
+    enable_auto_assignment = Column(Boolean, nullable=True)
 
     account = relationship("Account", back_populates="inboxes")
     messages = relationship(
@@ -36,7 +46,14 @@ class Inbox(BaseModel):
 
 class InboxMember(BaseModel):
     __tablename__ = "inbox_members"
-    __table_args__ = (Index("inbox_members_inbox_id_index", "inbox_id"),)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    __table_args__ = (
+        Index("inbox_members_inbox_id_index", "inbox_id"),
+        UniqueConstraint(
+            "user_id", "inbox_id", name="inbox_members_user_id_inbox_id_unique"
+        ),
+    )
+
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     inbox_id = Column(Integer, ForeignKey("inboxes.id"), nullable=False)
 
