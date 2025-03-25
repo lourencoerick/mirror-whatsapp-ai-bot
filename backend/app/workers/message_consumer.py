2563e9fed8bf0dc8036761db3d60d94141fc6787
Erick Lourenco <lourenco.erick@gmail.com>
2025-03-25 13:25:52 +0000

    helper init

--- backend/app/workers/message_consumer.py
+++ backend/app/workers/message_consumer.py
@@ -6,8 +6,10 @@ from sqlalchemy.orm import Session
 
 from app.database import SessionLocal
 from app.services.repository.message import get_or_create_message
+from app.services.repository.conversation import find_by_id as conversation_find_by_id
 from app.services.queue.redis_queue import RedisQueue
 from app.api.schemas.message_schema import MessageCreate
+from app.services.helper.conversation import update_last_message_snapshot
 
 
 class MessageConsumer:
@@ -68,8 +70,20 @@ class MessageConsumer:
             logger.warning(f"[consumer] Invalid message payload: {e}")
             return
 
-        saved = get_or_create_message(db=db, message_data=message_data)
-        if saved:
-            logger.info(f"[consumer] Message logged successfully: {saved.id}")
+        message = get_or_create_message(db=db, message_data=message_data)
+
+        if message:
+            conversation = conversation_find_by_id(
+                db=db, conversation_id=message.conversation_id
+            )
+
+            if conversation:
+                update_last_message_snapshot(conversation, message)
+            else:
+                logger.warning(
+                    f"[consumer] Conversation not found: {message.conversation_id}"
+                )
+
+            logger.info(f"[consumer] Message logged successfully: {message.id}")
         else:
             logger.warning("[consumer] Failed to save message")
