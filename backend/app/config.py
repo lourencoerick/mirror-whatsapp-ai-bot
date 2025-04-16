49191ce50ac5eabe0095b9a357865fddb1d6f1b9
Erick Lourenco <lourenco.erick@gmail.com>
2025-04-16 13:55:58 +0000

    feat: simplify the config file and use field to hide the password

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,6 +1,8 @@
+from sqlalchemy.engine.url import URL
 from pydantic_settings import BaseSettings
+from pydantic import Field
 from functools import lru_cache
-from typing import Optional, List
+from typing import Optional, List, 
 
 
 class Settings(BaseSettings):
@@ -12,21 +14,13 @@ class Settings(BaseSettings):
     BACKEND_BASE_URL: str = "http://localhost:8000"
 
     # --- Database ---
-    DATABASE_URL: str = (
-        "postgresql+asyncpg://user:password@whatsapp_bot_db_dev:5432/chatbotdb"
-    )
-    # DATABASE_URL: str = (
-    #     "postgresql+psycopg2://user:password@whatsapp_bot_db_dev:5432/chatbotdb"
-    # )
-    # DATABASE_URL=
-    # DATABASE_URL_: str = (
-    #     "postgresql+asyncpg://user:password@whatsapp_bot_db_dev:5432/chatbotdb"
-    # )
+    DATABASE_USER: str = "user"
+    DATABASE_PASSWORD: str = Field(..., env="DATABASE_PASSWORD")
+    DATABASE_HOST: str = "whatsapp_bot_db_dev"
+    DATABASE_PORT: int = 5432
+    DATABASE_NAME: str = "chatbotdb"
 
     # --- Redis ---
-    # Sugestão: Geralmente apenas REDIS_URL é suficiente, a menos que
-    # uma biblioteca específica precise dos componentes separados.
-    REDIS_URL: str = "redis://localhost:6379/0"
     REDIS_HOST: str = "redis"
     REDIS_PORT: int = 6379
     REDIS_DB: int = 0
@@ -34,10 +28,7 @@ class Settings(BaseSettings):
     # --- Evolution API ---
     EVOLUTION_API_KEY: str = "your-api-key"
     EVOLUTION_API_SHARED_URL: str = "http://localhost:8080"
-    EVOLUTION_INSTANCE: str = "680df327-c714-40a3-aec5-86ccbb57fa19"
-    SECRET_KEY_FOR_ENCRYPTION: str = (
-        "bBLiC4YQw25ISo2Ru58eckp86tFyVz7tj3mg6Q6N1bA="  # chave_secreta_forte_para_encriptacao_base64_aqui
-    )
+    SECRET_KEY_FOR_ENCRYPTION: str = Field(..., env="SECRET_KEY_FOR_ENCRYPTION")
 
     # --- Clerk ---
     CLERK_WEBHOOK_SECRET: str = "your-secret-key"
@@ -47,7 +38,7 @@ class Settings(BaseSettings):
 
     # --- Storage ---
     GCS_BUCKET_NAME: str = "wappbot-import-bucket"
-    GOOGLE_APPLICATION_CREDENTIALS: str = "credentials.json"
+    GOOGLE_APPLICATION_CREDENTIALS: str = Field(..., env="GOOGLE_APPLICATION_CREDENTIALS")
 
     FRONTEND_ALLOWED_ORIGINS: Optional[str] = None
 
@@ -55,10 +46,28 @@ class Settings(BaseSettings):
     APP_NAME: str = "WhatsApp AI Bot"
     DEBUG: bool = True
 
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return str(URL.create(
+            drivername="postgresql+asyncpg",
+            username=self.DATABASE_USER,
+            password=self.DATABASE_PASSWORD,
+            host=self.DATABASE_HOST,
+            port=self.DATABASE_PORT,
+            database=self.DATABASE_NAME,
+        ))    
+    
+    @property
+    def REDIS_URL(self) -> str:
+        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/{self.REDIS_DB}"
+    
     # --- Pydantic Settings Config ---
     class ConfigDict:
         case_sensitive = True
         extra = "ignore"
+
+
 
 
 @lru_cache()
