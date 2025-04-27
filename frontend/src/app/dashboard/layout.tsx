f48a966befc77f630d3420657c6a54cb45074adf
Erick Lourenco <lourenco.erick@gmail.com>
2025-04-27 00:44:56 +0000

    style(frontend): linter

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -1,25 +1,30 @@
 // app/dashboard/layout.tsx
-import { ReactNode } from "react";
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { DashboardShell } from "./shell";
+import { Toaster } from "@/components/ui/sonner";
 import { auth, currentUser } from "@clerk/nextjs/server";
+import { ReactNode } from "react";
+import { DashboardShell } from "./shell";
 
-export default async function DashboardLayout({ children }: { children: ReactNode }) {
-  const user = await currentUser()  
+export default async function DashboardLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const user = await currentUser();
 
-  await auth.protect(
-    () => user?.publicMetadata?.role === 'admin',
-    {
-      unauthorizedUrl:    '/pending-approval',
-    }
-  );  
+  await auth.protect(() => user?.publicMetadata?.role === "admin", {
+    unauthorizedUrl: "/pending-approval",
+  });
 
   console.log(`Dashboard admin acessado por ${user?.publicMetadata?.role}`);
   return (
     <SidebarProvider>
       <AppSidebar />
-      <DashboardShell>{children}</DashboardShell>
+      <DashboardShell>
+        {children}
+        <Toaster richColors position="top-right" />
+      </DashboardShell>
     </SidebarProvider>
   );
 }
