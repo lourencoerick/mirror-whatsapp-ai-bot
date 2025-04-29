6eb5bda9ddfc98316b66894ea8e67e75776d4440
Erick Lourenco <lourenco.erick@gmail.com>
2025-04-29 14:28:53 +0000

    feat(frontend): add logic to show the messages right when the user is on the simulation env or on the real env

--- frontend/src/components/ui/chat/chat-message.tsx
+++ frontend/src/components/ui/chat/chat-message.tsx
@@ -1,21 +1,31 @@
-import React, { forwardRef } from 'react';
-import { ChatBubble, ChatBubbleAvatar, ChatBubbleMessage } from "@/components/ui/chat/chat-bubble";
+import {
+  ChatBubble,
+  ChatBubbleAvatar,
+  ChatBubbleMessage,
+} from "@/components/ui/chat/chat-bubble";
+import { forwardRef } from "react";
 
 interface ChatMessageProps {
   content: string;
   direction: "in" | "out";
   loading?: boolean;
+  userDirection?: "in" | "out";
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const ChatMessage = forwardRef<HTMLDivElement, ChatMessageProps>(({ content, direction, loading = false }, ref) => {
-  return (
-    <ChatBubble variant={direction === "out" ? "sent" : "received"}>
-      <ChatBubbleAvatar src="" fallback={direction === "out" ? "🤖" : "👤"} />
-      <ChatBubbleMessage isLoading={loading}>{content}</ChatBubbleMessage>
-    </ChatBubble>
-  );
-}
+export const ChatMessage = forwardRef<HTMLDivElement, ChatMessageProps>(
+  ({ content, direction, loading = false, userDirection = "out" }, ref) => {
+    const isUserMsg = direction === userDirection;
+
+    const variant = isUserMsg ? "sent" : "received";
+    const avatarFallback = isUserMsg ? "👤" : "🤖";
+
+    return (
+      <ChatBubble variant={variant} ref={ref}>
+        <ChatBubbleAvatar src="" fallback={avatarFallback} />
+        <ChatBubbleMessage isLoading={loading}>{content}</ChatBubbleMessage>
+      </ChatBubble>
+    );
+  }
 );
 
 ChatMessage.displayName = "ChatMessage";
